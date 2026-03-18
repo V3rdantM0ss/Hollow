6f5e85fd532525a3171f737d27fae262559ce4ac
verdant_moss <V3rdantM0ss@gmx.de>
2026-03-18 21:49:57 +0100

    Fixed Color class which was royally TERRIBLE as you couldnt extract the color value. Oops

--- src/main/java/verdant_moss/hollow/Color.java
+++ src/main/java/verdant_moss/hollow/Color.java
@@ -2,16 +2,37 @@ package verdant_moss.hollow;
 
 public class Color {
 	public static final String RESET = "\u001B[0m";
-	private int red, green, blue, alpha;
+	public static final Color BLACK = new Color(0, 0, 0);
+	public static final Color DARK_RED = new Color(128, 0, 0);
+	public static final Color DARK_GREEN = new Color(0, 128, 0);
+	public static final Color DARK_YELLOW = new Color(128, 128, 0);
+	public static final Color DARK_BLUE = new Color(0, 0, 128);
+	public static final Color DARK_MAGENTA = new Color(128, 0, 128);
+	public static final Color DARK_CYAN = new Color(0, 128, 128);
+	public static final Color LIGHT_GRAY = new Color(192, 192, 192);
+	public static final Color GRAY = new Color(128, 128, 128);
+	public static final Color RED = new Color(255, 0, 0);
+	public static final Color GREEN = new Color(0, 255, 0);
+	public static final Color YELLOW = new Color(255, 255, 0);
+	public static final Color BLUE = new Color(0, 0, 255);
+	public static final Color MAGENTA = new Color(255, 0, 255);
+	public static final Color CYAN = new Color(0, 255, 255);
+	public static final Color WHITE = new Color(255, 255, 255);
+	public int red, green, blue, alpha;
 	
 	public Color() {
+		this(0, 0, 0, 255);
 	}
 	
-	public Color(int red, int green, int blue) {
+	public Color(int red, int green, int blue, int alpha) {
 		this.red = red;
 		this.green = green;
 		this.blue = blue;
-		this.alpha = 255;
+		this.alpha = alpha;
+	}
+	
+	public Color(int red, int green, int blue) {
+		this(red, green, blue, 255);
 	}
 	
 	public String toAnsi() {
--- src/main/java/verdant_moss/hollow/Hollow.java
+++ src/main/java/verdant_moss/hollow/Hollow.java
@@ -4,7 +4,7 @@ import verdant_moss.hollow.aurora.Aurora;
 
 public class Hollow {
 	public static final String HOLLOW_NAME = "Hollow";
-	public static final Version HOLLOW_VERSION = new Version(0, 0, 1, 3, ReleaseType.ALPHA);
+	public static final Version HOLLOW_VERSION = new Version(0, 0, 2, 0, ReleaseType.ALPHA);
 	public static final Color HOLLOW_COLOR = new Color(101, 0, 179);
 	public static final Hollow HOLLOW = new Hollow(HOLLOW_NAME, HOLLOW_VERSION, HOLLOW_COLOR);
 	private final String name;
